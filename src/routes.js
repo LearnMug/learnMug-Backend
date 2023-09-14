057be767257059a2d37f177453bab954ba101aed
Luana <santos.luanabeatriz1995@gmail.com>
2023-09-14 15:44:29 +0100

    Create CRUD da tabela message

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ function routes(app) {
     app.use('/course-professor', require('./routes/course_professor.js'));
     app.use('/course-students', require('./routes/course_students.js'));
     app.use('/message-recipient', require('./routes/message_recipient.js'));
+    app.use('/messages', require('./routes/messages.js'));
     return;
 }
 
---
+++ src/routes/messages.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const mysql = require("mysql2");
+const router = express.Router();
+router.use(express.json());
+
+router.post("/create", function (req, res) {
+  const connection = mysql.createConnection(process.env.DATABASE_URL);
+  const { subject, message, creating_user_id } = req.body;
+  const create_at = new Date();
+
+  sql =
+    "INSERT INTO messages (subject, message, creating_user_id, create_at) VALUES (?, ?, ?, ?)";
+  const values = [subject, message, creating_user_id, create_at];
+
+  connection.query(sql, values, (err, results) => {
+    if (err) {
+      console.error("Erro ao criar mensagem no banco de dados:", err);
+      return res.status(401).json({ error: err });
+    }
+
+    res.json({ message_id: results.insertId });
+  });
+});
+
+router.get("/get-messages", (req, res) => {
+  const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+  connection.query("SELECT * FROM messages", (err, results) => {
+    if (err) {
+      console.error("Erro na consulta ao banco de dados:", err);
+      return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+
+    res.json({ data: results });
+  });
+});
+
+router.get("/get-messages/:id", (req, res) => {
+  const connection = mysql.createConnection(process.env.DATABASE_URL);
+  const id = req.params.id;
+
+  connection.query("SELECT * FROM messages WHERE id = ?", [id], (err, results) => {
+    if (err) {
+      console.error("Erro na consulta ao banco de dados:", err);
+      return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+
+    res.json({ data: results });
+  });
+});
+
+router.put("/update/:id", (req, res) => {
+  const connection = mysql.createConnection(process.env.DATABASE_URL);
+  const id = req.params.id;
+  const updatedFields = req.body;
+
+  const sql = "UPDATE messages SET ? WHERE id = ?";
+  const values = [updatedFields, id];
+
+  connection.query(sql, values, (err, results) => {
+    if (err) {
+      console.error("Erro ao editar menssagem no banco de dados:", err);
+      return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+
+    res.json({ data: results });
+  });
+});
+
+router.delete("/delete/:id", (req, res) => {
+  const connection = mysql.createConnection(process.env.DATABASE_URL);
+  const id = req.params.id;
+
+  const sql = "DELETE FROM messages WHERE id = ?";
+
+  connection.query(sql, [id], (err, results) => {
+    if (err) {
+      console.error("Erro ao excluir menssagem do banco de dados:", err);
+      return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+
+    res.json({ data: results });
+  });
+});
+
+module.exports = router;
