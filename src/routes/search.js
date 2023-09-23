be87679837423332ce3870412e8b92ae51689f3a
Luana <santos.luanabeatriz1995@gmail.com>
2023-09-23 23:39:41 +0100

    Criada nova rota do search

--- src/routes/search.js
+++ src/routes/search.js
@@ -2,30 +2,30 @@ const express = require("express");
 const router = express.Router();
 const connectDB = require("../middleware/connectDB");
 router.use(express.json());
+const axios = require('axios');
 
-router.post("/get-courses-by-name", async (req, res) => {
+const BASE_URL = "https://learn-mug-backend.vercel.app/search"
+
+router.post("/", async (req, res) => {
+  const { name } = req.body;
+
+  const dados = {
+      name: name
+  }
+
+  const resultCoursesByName = await axios.post(`${BASE_URL}/get-courses-by-name-or-syllabus`, dados);
+  const resultCategoriesByName = await axios.post(`${BASE_URL}/get-categorie-by-name`, dados);
+
+  res.json({ data: {courses: resultCoursesByName.data, categories: resultCategoriesByName.data} });
+});
+
+router.post("/get-courses-by-name-or-syllabus", async (req, res) => {
   // #swagger.tags = ['Search']
   try {
     const connection = await connectDB();
     const { name } = req.body;
 
-    const [result] = await connection.query("SELECT * FROM view_courses WHERE name LIKE ?", [`%${name}%`]);
-    await connection.end();
-
-    res.json({ data: result });
-  } catch (error) {
-    console.error("Error in the database query:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-router.post("/get-course-by-syllabus", async (req, res) => {
-  // #swagger.tags = ['Search']
-  try {
-    const connection = await connectDB();
-    const { course_syllabus } = req.body;
-
-    const [result] = await connection.query("SELECT * FROM view_courses WHERE course_syllabus LIKE ?", [`%${course_syllabus}%`]);
+    const [result] = await connection.query("SELECT * FROM view_courses WHERE name LIKE ? OR course_syllabus LIKE ?", [`%${name}%`, `%${name}%`]);
     await connection.end();
 
     res.json({ data: result });
