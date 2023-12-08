a9721476f9e0e231fb62f5f8dea3f844a3f7d678
Luana <santos.luanabeatriz1995@gmail.com>
2023-12-08 14:15:32 +0000

    Update upload.js

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -42,4 +42,24 @@ router.post("/:id", async (req, res) => {
   });
 });
 
+router.get("/:id/image", async (req, res) => {
+  const id = req.params.id;
+
+  // Faça uma requisição para obter os dados do usuário com a imagem em base64
+  try {
+    const response = await axios.get(`${BASE_URL}/get/${id}`);
+    const userData = response.data;
+
+    // Verifique se o usuário possui a propriedade profile_img (imagem em base64)
+    if (userData.profile_img) {
+      // A imagem já está em base64, envie-a como resposta
+      res.status(200).send(userData.profile_img);
+    } else {
+      res.status(404).json({ error: "Imagem não encontrada para o usuário." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao recuperar a imagem do servidor." });
+  }
+});
+
 module.exports = router;
